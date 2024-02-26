c93bfbe8f72e1cfe1ad938afc3b6ef4ab09ba8da
Vladimir Vinogradov <vavinogradov@edu.hse.ru>
2024-02-27 00:01:01 +0300

    fix header hidden menu positioning

--- WEB_FOR_MSU/static/js/base/hidden_menu.js
+++ WEB_FOR_MSU/static/js/base/hidden_menu.js
@@ -5,22 +5,26 @@ var hidden_menu_courses = document.querySelector(".hidden_menu_courses");
 
 courses.addEventListener("mouseover", function () {
         hidden_menu_courses.style.visibility = "visible";
+        courses.classList.add("header_button_mouseon");
     }
 
 );
 
 hidden_menu_courses.addEventListener("mouseover", function () {
     hidden_menu_courses.style.visibility = "visible";
+    courses.classList.add("header_button_mouseon");
 });
 
 courses.addEventListener("mouseout", function () {
         hidden_menu_courses.style.visibility = "hidden";
+        courses.classList.remove("header_button_mouseon");
     }
 
 );
 
 hidden_menu_courses.addEventListener("mouseout", function () {
     hidden_menu_courses.style.visibility = "hidden";
+    courses.classList.remove("header_button_mouseon");
 });
 
 
@@ -30,22 +34,26 @@ var hidden_menu_events = document.querySelector(".hidden_menu_events");
 
 events.addEventListener("mouseover", function () {
         hidden_menu_events.style.visibility = "visible";
+        events.classList.add("header_button_mouseon");
     }
 
 );
 
 hidden_menu_events.addEventListener("mouseover", function () {
     hidden_menu_events.style.visibility = "visible";
+    events.classList.add("header_button_mouseon");
 });
 
 events.addEventListener("mouseout", function () {
         hidden_menu_events.style.visibility = "hidden";
+        events.classList.remove("header_button_mouseon");
     }
 
 );
 
 hidden_menu_events.addEventListener("mouseout", function () {
     hidden_menu_events.style.visibility = "hidden";
+    events.classList.remove("header_button_mouseon");
 });
 
 
@@ -57,20 +65,24 @@ var hidden_menu_community = document.querySelector(".hidden_menu_community");
 
 community.addEventListener("mouseover", function () {
         hidden_menu_community.style.visibility = "visible";
+        community.classList.add("header_button_mouseon");
     }
 
 );
 
 hidden_menu_community.addEventListener("mouseover", function () {
     hidden_menu_community.style.visibility = "visible";
+    community.classList.add("header_button_mouseon");
 });
 
 community.addEventListener("mouseout", function () {
         hidden_menu_community.style.visibility = "hidden";
+        community.classList.remove("header_button_mouseon");
     }
 
 );
 
 hidden_menu_community.addEventListener("mouseout", function () {
     hidden_menu_community.style.visibility = "hidden";
+    community.classList.remove("header_button_mouseon");
 });
