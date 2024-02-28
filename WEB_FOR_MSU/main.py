8adb9e5b04e8721a6a98681de05b28a67848be3b
Vladimir Vinogradov <vavinogradov@edu.hse.ru>
2024-02-28 20:19:51 +0300

    create registration and account pages, add entrance page and some things to change account info

--- WEB_FOR_MSU/main.py
+++ WEB_FOR_MSU/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
+from PIL import Image
 import os
 
 load_dotenv()
@@ -13,12 +14,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (f'postgresql+psycopg2://'
                                          + f'{os.getenv("DB_PORT")}/'
                                          + f'{os.getenv("DB_NAME")}')
 
-
 # db = SQLAlchemy(app)
 
+PEOPLE_FOLDER = os.path.join('static', 'photos', 'people_photo')
 
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
+
+
+@app.route('/')
 @app.route('/home')
-def index():
+def home():
     user = {'name': 'Vladimir',
             'surname': 'Vinogradov',
             'status': 'Teacher',
@@ -28,11 +34,24 @@ def index():
                            user=user)
 
 
-@app.route('/account')
-def schedule():
+@app.route('/registration')
+def registration():
     user = {'name': 'Vladimir',
             'surname': 'Vinogradov',
             'status': 'Teacher',
+            }
+    return render_template('registration.html',
+                           title='Registration',
+                           user=user)
+
+
+@app.route('/account')
+def account():
+    image = os.path.join(app.config['UPLOAD_FOLDER'], 'Me in msu.jpg')
+    user = {'name': 'Vladimir',
+            'surname': 'Vinogradov',
+            'status': 'Teacher',
+            'photo': image,
             }
     return render_template('account.html',
                            title='Account',
