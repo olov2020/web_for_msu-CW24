f0ff4469ccccc802c92735aba993337c56aef99d
Vladimir Vinogradov <vavinogradov@edu.hse.ru>
2024-03-04 18:33:42 +0300

    working on login

--- WEB_FOR_MSU/static/js/base/hidden_menu.js
+++ WEB_FOR_MSU/static/js/base/hidden_menu.js
@@ -1,34 +1,45 @@
 "use strict";
 
 window.onload = function() {
+    var hidden_menu = document.querySelector('.hidden_menu');
+    hidden_menu.addEventListener('mouseout', function() {
+        hidden_menu.style.zIndex = 1;
+    });
+    
     var hidden_menu_courses = document.querySelector(".hidden_menu_courses");
 
     hidden_menu_courses.addEventListener("mouseover", function () {
         hidden_menu_courses.style.visibility = "visible";
+        hidden_menu_courses.style.zIndex = 10;
     });
 
     hidden_menu_courses.addEventListener("mouseout", function () {
         hidden_menu_courses.style.visibility = "hidden";
+        hidden_menu_courses.style.zIndex = 1;
     });
 
     var hidden_menu_events = document.querySelector(".hidden_menu_events");
 
     hidden_menu_events.addEventListener("mouseover", function () {
         hidden_menu_events.style.visibility = "visible";
+        hidden_menu_events.style.zIndex = 10;
     });
 
     hidden_menu_events.addEventListener("mouseout", function () {
         hidden_menu_events.style.visibility = "hidden";
+        hidden_menu_events.style.zIndex = 1;
     });
 
     var hidden_menu_community = document.querySelector(".hidden_menu_community");
 
     hidden_menu_community.addEventListener("mouseover", function () {
         hidden_menu_community.style.visibility = "visible";
+        hidden_menu_community.style.zIndex = 10;
     });
 
     hidden_menu_community.addEventListener("mouseout", function () {
         hidden_menu_community.style.visibility = "hidden";
+        hidden_menu_community.style.zIndex = 1;
     });
 
 
@@ -36,30 +47,36 @@ window.onload = function() {
 
     menu_courses.addEventListener("mouseover", function () {
         hidden_menu_courses.style.visibility = "visible";
+        hidden_menu_courses.style.zIndex = 10;
     });
 
     menu_courses.addEventListener("mouseout", function () {
         hidden_menu_courses.style.visibility = "hidden";
+        hidden_menu_courses.style.zIndex = 1;
     });
 
     var menu_events = document.querySelector(".events");
 
     menu_events.addEventListener("mouseover", function () {
         hidden_menu_events.style.visibility = "visible";
+        hidden_menu_events.style.zIndex = 10;
     });
 
     menu_events.addEventListener("mouseout", function () {
         hidden_menu_events.style.visibility = "hidden";
+        hidden_menu_events.style.zIndex = 1;
     });
 
     var menu_community = document.querySelector(".community");
 
     menu_community.addEventListener("mouseover", function () {
         hidden_menu_community.style.visibility = "visible";
+        hidden_menu_community.style.zIndex = 10;
     });
 
     menu_community.addEventListener("mouseout", function () {
         hidden_menu_community.style.visibility = "hidden";
+        hidden_menu_community.style.zIndex = 1;
     });
 
 }
