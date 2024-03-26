68aebdec3ec80131b659d92c7063c5ebe5254505
Dmitry Mukhin <dvmukhin@edu.hse.ru>
2024-03-26 10:46:51 +0300

    fixed new_password

--- WEB_FOR_MSU/forms/account.py
+++ WEB_FOR_MSU/forms/account.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import Form, ValidationError
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, PasswordField
 from wtforms.fields.simple import EmailField, TelField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, Optional
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 
@@ -12,7 +12,7 @@ class AccountForm(FlaskForm):
     patronymic = StringField("Отчество: ")
     email = EmailField("Email: ", validators=[Email()])
     password = PasswordField("Пароль: ", validators=[Length(min=4, max=100)])
-    new_password = PasswordField("Новый пароль: ")
+    new_password = PasswordField("Новый пароль: ", validators=[Optional(), Length(min=4, max=100)])
     phone = TelField("Телефон: ")
     school = StringField("Школа: ")
     image = FileField("Обновить фото профиля", validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif', 'bmp'])])
