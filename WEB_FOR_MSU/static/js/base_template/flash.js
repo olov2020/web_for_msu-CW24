c18680eb2e2708e7052f0f4aa28319a0157fd956
Vladimir Vinogradov <vavinogradov@edu.hse.ru>
2024-03-27 13:42:36 +0300

    add flash to account page, fix value of inputs

--- WEB_FOR_MSU/static/js/base_template/flash.js
+++ WEB_FOR_MSU/static/js/base_template/flash.js
@@ -1,11 +1,11 @@
 'use strict';
 
-window.onload = function() {
+window.onload = function () {
     var inputs = document.querySelectorAll('input[type=email], input[type=text], input[type=password], input[type=phone]');
     var flash_message = document.getElementsByClassName('flash_message');
-    
+
     for (var i = 0; i < inputs.length; ++i) {
-        inputs[i].addEventListener('click', function(){
+        inputs[i].addEventListener('click', function () {
             flash_message[0].style.visibility = 'hidden';
             flash_message[0].style.display = 'none';
         });
