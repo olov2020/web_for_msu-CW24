1e641f99fff6252bf55c980dd53fb8bfe3a96006
Vladimir Vinogradov <vavinogradov@edu.hse.ru>
2024-03-27 13:24:11 +0300

    handle flash messages

---
+++ WEB_FOR_MSU/static/js/base_template/flash.js
@@ -0,0 +1,13 @@
+'use strict';
+
+window.onload = function() {
+    var inputs = document.querySelectorAll('input[type=email], input[type=text], input[type=password], input[type=phone]');
+    var flash_message = document.getElementsByClassName('flash_message');
+    
+    for (var i = 0; i < inputs.length; ++i) {
+        inputs[i].addEventListener('click', function(){
+            flash_message[0].style.visibility = 'hidden';
+            flash_message[0].style.display = 'none';
+        });
+    }
+}
