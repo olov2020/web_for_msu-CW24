e9723e387c5755a9a2eb8253d7a3c355b9d585c0
Dmitry Mukhin <dvmukhin@edu.hse.ru>
2024-03-31 14:12:21 +0300

    fixed bugs

--- WEB_FOR_MSU/forms/course_file.py
+++ WEB_FOR_MSU/forms/course_file.py
@@ -8,6 +8,6 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 
 class CourseFileForm(FlaskForm):
-    file = FileField("Файл с курсом. Доступные форматы: '.xls'. *",
-                     validators=[FileAllowed(['xls'], message="Некорректный формат файла")])
+    file = FileField("Файл с курсом. Доступные форматы: '.xls', '.xlsx'. *",
+                     validators=[FileAllowed(['xls', 'xlsx'], message="Некорректный формат файла")])
     load_submit = SubmitField("Загрузить курс")
