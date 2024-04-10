c3fb2536358f10574008657e4979358bcd809ce5
Dmitry Mukhin <dvmukhin@edu.hse.ru>
2024-04-10 12:59:54 +0300

    trying to optimize save_from_form

--- WEB_FOR_MSU/services/mark_service.py
+++ WEB_FOR_MSU/services/mark_service.py
@@ -1,3 +1,8 @@
+import itertools
+from operator import attrgetter
+
+from sqlalchemy import asc
+
 from WEB_FOR_MSU import db
 from WEB_FOR_MSU.models import Mark, Course, Schedule, Formula
 
@@ -27,19 +32,30 @@ class MarkService:
 
     @staticmethod
     def save_from_form(course_id, marks_form):
+        lessons = Schedule.query.filter_by(course_id=course_id).order_by(Schedule.date).all()
+        formula_vals = Formula.query.filter_by(course_id=course_id).all()
+        formulas = {formula.name: formula for formula in formula_vals}
         for i in range(len(marks_form.dates)):
             if marks_form.mark_types[i].data != 'Отсутствие':
-                lesson = Schedule.query.filter_by(course_id=course_id, date=marks_form.dates[i].data).first()
-                lesson.formulas = Formula.query.filter_by(course_id=course_id, name=marks_form.mark_types[i].data).first()
+                lesson = lessons[i]
+                lesson.formulas = formulas[marks_form.mark_types[i].data]
+        marks = Mark.query.order_by(Mark.pupil_id, asc(Mark.schedule_id)).all()
+        marks_grouped = {}
+        for key, group in itertools.groupby(marks, key=attrgetter('pupil_id')):
+            marks_grouped[str(key)] = {mark.schedule_id: mark for mark in group}
         for i in range(len(marks_form.pupils)):
             for j in range(len(marks_form.dates)):
                 mark = marks_form.pupils[i].form.marks[j].data
-                if mark:
-                    lesson = Schedule.query.filter_by(course_id=course_id, date=marks_form.dates[j].data).first()
-                    prev_mark = Mark.query.filter_by(schedule_id=lesson.id, pupil_id=marks_form.pupils[i].form.id.data).first()
-                    if prev_mark:
+                lesson = lessons[j]
+                prev_mark = marks_grouped[marks_form.pupils[i].form.id.data][lesson.id] if (
+                        marks_form.pupils[i].form.id.data in marks_grouped and lesson.id in
+                        marks_grouped[marks_form.pupils[i].form.id.data]) else None
+                if prev_mark:
+                    if mark:
                         prev_mark.mark = mark
                     else:
-                        new_mark = Mark(lesson.id, marks_form.pupils[i].form.id.data, mark, None)
-                        db.session.add(new_mark)
+                        db.session.delete(prev_mark)
+                elif mark:
+                    new_mark = Mark(lesson.id, marks_form.pupils[i].form.id.data, mark, None)
+                    db.session.add(new_mark)
         db.session.commit()
