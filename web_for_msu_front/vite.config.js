f6b98d77787fe6b4db34b1ec0ec3bd3ea9ab2798
olov2020 <vladimir.8624@mail.ru>
2024-11-11 23:02:21 +0300

    make config for front

--- web_for_msu_front/vite.config.js
+++ web_for_msu_front/vite.config.js
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  build: {
+    assetsDir: 'static',
+  },
+  server: {
+    port: 5173,
+    cors: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:5000/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
