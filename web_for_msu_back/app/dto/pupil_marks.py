f85ed85b5313487764a8a5bc45ea2c93135a74b5
Dmitry-Pr <muhineima@gmail.com>
2024-11-23 15:58:03 +0300

    fixed pupilMarksDTO

--- web_for_msu_back/app/dto/pupil_marks.py
+++ web_for_msu_back/app/dto/pupil_marks.py
@@ -5,6 +5,6 @@ class PupilMarksDTO(Schema):
     course_name = fields.String(required=True)
     dates = fields.List(fields.String(), required=True)
     mark_types = fields.List(fields.String(), required=True)
-    marks = fields.List(fields.Integer(), required=True)
+    marks = fields.List(fields.String(), required=True)
     skips = fields.Int(required=True)
     result = fields.Float(required=True)
