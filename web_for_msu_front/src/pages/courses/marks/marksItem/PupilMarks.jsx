508e31b25bf164690aadd85308af745447f42fff
olov2020 <vladimir.8624@mail.ru>
2024-11-24 19:01:11 +0300

    working on marks

--- web_for_msu_front/src/pages/courses/marks/marksItem/PupilMarks.jsx
+++ web_for_msu_front/src/pages/courses/marks/marksItem/PupilMarks.jsx
@@ -5,13 +5,13 @@ import {getPupilMarksByCourseId} from "../../../../api/coursesApi.js";
 // eslint-disable-next-line react/prop-types
 const PupilMarks = ({courseId}) => {
 
-  const [dates, setDates] = useState([]);
+  const [dates, setDates] = useState(['as', 'asd', 'asd']);
   const [markTypes, setMarkTypes] = useState([]);
-  const [marks, setMarks] = useState([]);
-  const [result, setResult] = useState('');
-  const [skips, setSkips] = useState('');
+  const [marks, setMarks] = useState(['ke', 'we', 'we']);
+  const [result, setResult] = useState('1243');
+  const [skips, setSkips] = useState('as');
 
-  useEffect(() => {
+  /*useEffect(() => {
     const getMarks = async () => {
       const data = await getPupilMarksByCourseId({courseId});
       setDates(data.dates);
@@ -22,53 +22,41 @@ const PupilMarks = ({courseId}) => {
     }
 
     getMarks();
-  }, [])
+  }, [])*/
 
   return (
-      <table key={courseId}>
-        <caption>
-          Ведомость оценок
-        </caption>
+    <table key={courseId}>
+      <caption>
+        <h3>Ведомость оценок</h3>
+      </caption>
 
-        <thead>
-        <tr>
-          <th scope="col"></th>
-          {dates.map((date) => {
-            <th scope="col">{date}</th>
-          })}
-        </tr>
-        </thead>
+      <thead>
+      <tr>
+        {dates.map((date, index) => {
+          <th key={index}>{date}</th>
+        })}
+      </tr>
+      </thead>
 
-        <tbody>
-        <tr>
-          <th scope="row">Chris</th>
-          <td>HTML tables</td>
-          <td>22</td>
-        </tr>
-        <tr>
-          <th scope="row">Dennis</th>
-          <td>Web accessibility</td>
-          <td>45</td>
-        </tr>
-        <tr>
-          <th scope="row">Sarah</th>
-          <td>JavaScript frameworks</td>
-          <td>29</td>
-        </tr>
-        <tr>
-          <th scope="row">Karen</th>
-          <td>Web performance</td>
-          <td>36</td>
-        </tr>
-        </tbody>
+      <tbody>
+      <tr>
+        {marks.map((mark, index) => {
+          <td key={index}>{mark}</td>
+        })}
+      </tr>
+      </tbody>
 
-        <tfoot>
-        <tr>
-          <th scope="row" colSpan="2">Average age</th>
-          <td>33</td>
-        </tr>
-        </tfoot>
-      </table>
+      <tfoot>
+      <tr>
+        <th scope="row" colSpan={dates.length - 1}>Итоговая оценка</th>
+        <td>{result}</td>
+      </tr>
+      <tr>
+        <th scope="row" colSpan={dates.length - 1}>Общее количество пропусков</th>
+        <td>{skips}</td>
+      </tr>
+      </tfoot>
+    </table>
   );
 };
 
