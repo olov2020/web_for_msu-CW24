8900ea60cf40dd5b734ad50606a3d6a339aa992a
Dmitry-Pr <muhineima@gmail.com>
2024-11-26 00:30:33 +0300

    added credit for schedule, added lessons to courses view

--- web_for_msu_back/app/dto/course_info_pupil.py
+++ web_for_msu_back/app/dto/course_info_pupil.py
@@ -5,3 +5,4 @@ from web_for_msu_back.app.dto.course_info import CourseInfoDTO
 
 class CourseInfoPupilDTO(CourseInfoDTO):
     current_mark = fields.Float(allow_none=True)
+    credit = fields.String()
