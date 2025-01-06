981c0dc0472616a05c6fabeb99bd6cace6f41cf9
olov2020 <vladimir.8624@mail.ru>
2025-01-06 19:25:15 +0300

    working on frontend and backend showing dialog window

--- web_for_msu_front/src/generic/header/developers/Backend.jsx
+++ web_for_msu_front/src/generic/header/developers/Backend.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import style from './developers.module.css';
+import closeIcon from '../../../../public/generic/closeIcon.svg'
 
-const Backend = () => {
+// eslint-disable-next-line react/prop-types
+const Backend = ({setShowFrontend}) => {
   return (
-    <div>
-      
-    </div>
+    <section className={style.section}>
+      <h3>Привет, уважаемый посетитель сайта!</h3>
+
+      <p>Меня зовут Виноградов Владимир, я frontend разработчик данного сайта. Был преподавателем ЭМШ на протяжении 2
+        лет и посетил 5 Летних школ - от 8 класса до преподавателя</p>
+
+      <img src={closeIcon} alt='Закрыть окно' onClick={() => setShowFrontend(false)}/>
+    </section>
   );
 };
 
--- web_for_msu_front/src/generic/header/developers/Frontend.jsx
+++ web_for_msu_front/src/generic/header/developers/Frontend.jsx
@@ -1,13 +1,15 @@
 import style from './developers.module.css';
 import closeIcon from '../../../../public/generic/closeIcon.svg'
 
+// eslint-disable-next-line react/prop-types
 const Frontend = ({setShowFrontend}) => {
   return (
     <section className={style.section}>
       <h3>Привет, уважаемый посетитель сайта!</h3>
 
-      <p>Меня зовут Виноградов Владимир и я являюсь frontend разработчиком этого сайта.</p>
-      <p>Меня зовут Виноградов Владимир и я являюсь frontend разработчиком этого сайта.</p>
+      <p>Меня зовут Виноградов Владимир, я frontend разработчик данного сайта - отвечаю за дизайн и пользовательский опыт.
+        Был преподавателем ЭМШ на протяжении 2 лет и посетил 5 Летних школ - от 8 класса до преподавателя. Сайт
+      разработан в 2024-2025 годах в рамках Курсового проекта Высшей школы экономики. </p>
 
       <img src={closeIcon} alt='Закрыть окно' onClick={() => setShowFrontend(false)}/>
     </section>
