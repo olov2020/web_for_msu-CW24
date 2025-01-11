4043459f0de7f9f011e42fa7a7b8d8ef2116f251
Dmitry-Pr <muhineima@gmail.com>
2025-01-11 22:04:39 +0300

    changed date format for events

--- web_for_msu_back/app/dto/contest_scientific_works_dates.py
+++ web_for_msu_back/app/dto/contest_scientific_works_dates.py
@@ -1,7 +1,21 @@
-from marshmallow import Schema, fields
+from datetime import datetime
+
+from marshmallow import Schema, fields, post_dump
 
 
 class ContestScientificWorksDatesDTO(Schema):
-    date_first = fields.Date(format="%d.%m.%Y")
-    date_second = fields.Date(format="%d.%m.%Y")
-    date_third = fields.Date(format="%d.%m.%Y")
+    date_first = fields.Date()
+    date_second = fields.Date()
+    date_third = fields.Date()
+
+    @post_dump
+    def format_dates(self, data, **kwargs):
+        date_format = "%d.%m.%Y"
+        for key in ["date_first", "date_second", "date_third"]:
+            if key in data and data[key]:
+                # Преобразование строки 'YYYY-MM-DD' к объекту datetime и форматирование
+                if isinstance(data[key], str):
+                    data[key] = datetime.strptime(data[key], "%Y-%m-%d").strftime(date_format)
+                elif isinstance(data[key], datetime):
+                    data[key] = data[key].strftime(date_format)
+        return data
