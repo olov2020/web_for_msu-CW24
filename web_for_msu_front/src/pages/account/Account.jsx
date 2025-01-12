41cca56b96ac28e69cb90086b860b135edcefe70
olov2020 <vladimir.8624@mail.ru>
2025-01-12 16:50:06 +0300

    fix

--- web_for_msu_front/src/pages/account/Account.jsx
+++ web_for_msu_front/src/pages/account/Account.jsx
@@ -14,6 +14,7 @@ const Account = () => {
   useEffect(() => {
     const getUserDataFunc = async () => {
       const data = authStatus.includes('pupil') ? await getPupilInfo() : await getTeacherInfo();
+      console.log(data)
       setUser(JSON.parse(data));
     }
 
