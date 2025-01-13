71a30d01df6337352dac6653b1216005ea6f3260
olov2020 <vladimir.8624@mail.ru>
2025-01-13 16:12:05 +0300

    fix

--- web_for_msu_front/src/generic/form/Form.jsx
+++ web_for_msu_front/src/generic/form/Form.jsx
@@ -423,7 +423,7 @@ const Form = ({inputs = [], values = {}, buttonText, type, id = undefined}) => {
         formErrors.work = error;
         return <InputText name={input} placeholder='Введите свое место работы' formErrors={setError}
                           fieldName='Место работы'
-                          value={work} setValue={setWork}
+                          value={'wjowienoweiw'} setValue={setWork}
         />
       }
       case 'telegram': {
