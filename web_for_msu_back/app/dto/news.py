ab6d7fed76e85993db1108e99e815410d9153d30
Dmitry-Pr <muhineima@gmail.com>
2025-01-13 15:53:16 +0300

    fixed creating news, if the request doesn't contain a photo, it is set to default

--- web_for_msu_back/app/dto/news.py
+++ web_for_msu_back/app/dto/news.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 import pytz
-from marshmallow import Schema, fields, validates, ValidationError, post_load
+from marshmallow import Schema, fields, validates, ValidationError, post_load, post_dump
 
 from web_for_msu_back.app.models.news import News
 
@@ -10,8 +10,7 @@ class NewsDTO(Schema):
     id = fields.Integer(dump_only=True)  # Только для чтения, так как `id` генерируется автоматически
     title = fields.String(required=True, validate=lambda x: len(x) > 0)  # Заголовок обязателен и не может быть пустым
     description = fields.String()
-    date = fields.DateTime(default=lambda: datetime.now(tz=pytz.timezone('Europe/Moscow')).date(),
-                           format='%Y-%m-%d')
+    date = fields.DateTime(dump_only=True, default=lambda: datetime.now(tz=pytz.timezone('Europe/Moscow')).date())
     photo = fields.String(required=True, validate=lambda x: len(x) > 0)  # Фото обязательно и не может быть пустым
 
     @validates("date")
@@ -28,3 +27,15 @@ class NewsDTO(Schema):
     @post_load
     def make_news(self, data, **kwargs):
         return News(**data)
+
+    @post_dump
+    def format_dates(self, data, **kwargs):
+        date_format = "%d.%m.%Y"
+        for key in ["date"]:
+            if key in data and data[key]:
+                # Преобразование строки 'YYYY-MM-DD' к объекту datetime и форматирование
+                if isinstance(data[key], str):
+                    data[key] = datetime.strptime(data[key], "%Y-%m-%d").strftime(date_format)
+                elif isinstance(data[key], datetime):
+                    data[key] = data[key].strftime(date_format)
+        return data
