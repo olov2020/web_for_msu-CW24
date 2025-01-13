f39db5dde764ee73dc129619374731321eb5e710
olov2020 <vladimir.8624@mail.ru>
2025-01-13 12:05:41 +0300

    working on teachers list in admin panel

--- web_for_msu_front/src/pages/adminPanel/listsOf/ListOfPeople.jsx
+++ web_for_msu_front/src/pages/adminPanel/listsOf/ListOfPeople.jsx
@@ -105,28 +105,83 @@ const ListOfPeople = () => {
 
   const addAdminRole = async (userId, funcName) => {
     switch (funcName) {
-      case 'setNewsAdmin':
-        return await setNewsAdmin(userId);
-      case 'setCourseAdmin':
-        return await setCourseAdmin(userId);
-      case 'setMarksAdmin':
-        return await setMarksAdmin(userId);
-      case 'setAuditoryAdmin':
-        return await setAuditoryAdmin(userId);
-      case 'setKNRAdmin':
-        return await setKNRAdmin(userId);
-      case 'setVSHAdmin':
-        return await setVSHAdmin(userId);
-      case 'setLSHAdmin':
-        return await setLSHAdmin(userId);
-      case 'setTestsOfflineAdmin':
-        return await setTestsOfflineAdmin(userId);
-      case 'setTestsOnlineAdmin':
-        return await setTestsOnlineAdmin(userId);
-      case 'setDirectoryTeacher':
-        return await setDirectoryTeacher(userId);
-      case 'setSovetTeacher':
-        return await setSovetTeacher(userId);
+      case 'setNewsAdmin': {
+        const data = await setNewsAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'setCourseAdmin': {
+        const data = await setCourseAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'setMarksAdmin': {
+        const data = await setMarksAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'setAuditoryAdmin': {
+        const data = await setAuditoryAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'setKNRAdmin': {
+        const data = await setKNRAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'setVSHAdmin': {
+        const data = await setVSHAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'setLSHAdmin': {
+        const data = await setLSHAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'setTestsOfflineAdmin': {
+        const data = await setTestsOfflineAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'setTestsOnlineAdmin': {
+        const data = await setTestsOnlineAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'setDirectoryTeacher': {
+        const data = await setDirectoryTeacher(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'setSovetTeacher': {
+        const data = await setSovetTeacher(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
       default:
         return Promise.reject(new Error('Invalid function name'));
     }
@@ -134,28 +189,83 @@ const ListOfPeople = () => {
 
   const deleteAdminRole = async (userId, funcName) => {
     switch (funcName) {
-      case 'deleteNewsAdmin':
-        return await deleteNewsAdmin(userId);
-      case 'deleteCourseAdmin':
-        return await deleteCourseAdmin(userId);
-      case 'deleteMarksAdmin':
-        return await deleteMarksAdmin(userId);
-      case 'deleteAuditoryAdmin':
-        return await deleteAuditoryAdmin(userId);
-      case 'deleteKNRAdmin':
-        return await deleteKNRAdmin(userId);
-      case 'deleteVSHAdmin':
-        return await deleteVSHAdmin(userId);
-      case 'deleteLSHAdmin':
-        return await deleteLSHAdmin(userId);
-      case 'deleteTestsOfflineAdmin':
-        return await deleteTestsOfflineAdmin(userId);
-      case 'deleteTestsOnlineAdmin':
-        return await deleteTestsOnlineAdmin(userId);
-      case 'deleteDirectoryTeacher':
-        return await deleteDirectoryTeacher(userId);
-      case 'deleteSovetTeacher':
-        return await deleteSovetTeacher(userId);
+      case 'deleteNewsAdmin': {
+        const data = await deleteNewsAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'deleteCourseAdmin': {
+        const data = await deleteCourseAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'deleteMarksAdmin': {
+        const data = await deleteMarksAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'deleteAuditoryAdmin': {
+        const data = await deleteAuditoryAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'deleteKNRAdmin': {
+        const data = await deleteKNRAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'deleteVSHAdmin': {
+        const data = await deleteVSHAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'deleteLSHAdmin': {
+        const data = await deleteLSHAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'deleteTestsOfflineAdmin': {
+        const data = await deleteTestsOfflineAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'deleteTestsOnlineAdmin': {
+        const data = await deleteTestsOnlineAdmin(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'deleteDirectoryTeacher': {
+        const data = await deleteDirectoryTeacher(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
+      case 'deleteSovetTeacher': {
+        const data = await deleteSovetTeacher(userId);
+        if (data) {
+          setUpdateList(!updateList);
+        }
+        return;
+      }
       default:
         return Promise.reject(new Error('Invalid function name'));
     }
