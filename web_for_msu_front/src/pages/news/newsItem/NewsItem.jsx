65d6f392c5fa2e694be19e535c7fbee5b2fb5405
olov2020 <vladimir.8624@mail.ru>
2025-01-17 18:58:09 +0300

    fixing downloading news file

--- web_for_msu_front/src/pages/news/newsItem/NewsItem.jsx
+++ web_for_msu_front/src/pages/news/newsItem/NewsItem.jsx
@@ -4,7 +4,6 @@ import DeleteWindow from "./deleteWindow/DeleteWindow.jsx";
 import {useState} from "react";
 import {useSelector} from "react-redux";
 import ButtonSubmit from "../../../generic/form/submit/ButtonSubmit.jsx";
-import axios from "axios";
 
 const NewsItem = () => {
 
@@ -14,16 +13,9 @@ const NewsItem = () => {
 
   const handleFileDownload = async (fileUrl, fileName) => {
     try {
-      const response = await axios.get(fileUrl, {
-        responseType: 'blob', // Important to set the response type to 'blob'
-      });
-
-      // Create a Blob from the response data
-      const blob = new Blob([response.data], { type: response.headers['content-type']  });
-
       // Create a temporary anchor element
       const link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
+      link.href = fileUrl;
       link.download = fileName; // Set the desired file name
 
       // Append the anchor to the document body
@@ -34,9 +26,6 @@ const NewsItem = () => {
 
       // Remove the anchor from the document body
       document.body.removeChild(link);
-
-      // Revoke the object URL to free up memory
-      window.URL.revokeObjectURL(link.href);
       alert('Файл успешно скачан!');
     } catch {
       alert('Упс... что-то пошло не так. Файл не был скачан!');
