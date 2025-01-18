331ae077bf9c9e7e35faa8751049da6c59412f8a
olov2020 <vladimir.8624@mail.ru>
2025-01-19 00:43:06 +0300

    working on styling pupil marks

--- web_for_msu_front/src/pages/courses/marks/PupilMarks.jsx
+++ web_for_msu_front/src/pages/courses/marks/PupilMarks.jsx
@@ -80,20 +80,14 @@ const PupilMarks = ({courseId}) => {
         </section>
 
         <section className={style.markTypesSection}>
-          {marks.mark_type_choices && marks.mark_type_choices.length !== 0 && marks.mark_type_choices.map((markType) => (
-            <section key={markType} className={style.markType}>
+          {marks.mark_type_choices && marks.mark_type_choices.length !== 0 && marks.mark_type_choices.map((markType, index) => (
+            <div key={markType} className={style.markType}>
               <h3>{markType}</h3>
 
-              <section className={style.marks}>
-                {marks.marks && marks.marks.length !== 0 && marks.marks.map((markArray, index) => (
-                  <div key={index}>
-                    {markArray.map((mark) => (
-                      <p key={mark}>{mark}</p>
-                    ))}
-                  </div>
-                ))}
-              </section>
-            </section>
+              {marks.marks && marks.marks.length !== 0 && marks.marks.map((markArray) => (
+                <p key={markArray}>{markArray[index]}</p>
+              ))}
+            </div>
           ))}
         </section>
 
