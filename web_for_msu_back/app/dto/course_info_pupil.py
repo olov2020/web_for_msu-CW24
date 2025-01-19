b939d329819e92fc11935b3b22505f27a1b27b94
Dmitry-Pr <muhineima@gmail.com>
2025-01-19 05:56:24 +0300

    fixed adding pupils to courses, now deleting crediting as well, changed current mark to string

--- web_for_msu_back/app/dto/course_info_pupil.py
+++ web_for_msu_back/app/dto/course_info_pupil.py
@@ -4,7 +4,7 @@ from web_for_msu_back.app.dto.course_info import CourseInfoDTO
 
 
 class CourseInfoPupilDTO(CourseInfoDTO):
-    current_mark = fields.Float(allow_none=True)
-    mark1 = fields.Integer(allow_none=True)
-    mark2 = fields.Integer(allow_none=True)
+    current_mark = fields.String(allow_none=True)
+    mark1 = fields.String(allow_none=True)
+    mark2 = fields.String(allow_none=True)
     credit = fields.String()
