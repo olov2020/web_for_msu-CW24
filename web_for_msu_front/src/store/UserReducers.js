cd65827bfbc006dffc51804c3e0b8f7f7d1c065a
olov2020 <vladimir.8624@mail.ru>
2025-01-24 19:30:13 +0300

    put user photo into local storage

--- web_for_msu_front/src/store/UserReducers.js
+++ web_for_msu_front/src/store/UserReducers.js
@@ -54,13 +54,50 @@ export const setNotAuthAction = (payload) => ({type: SET_NOTAUTH, payload})
 
 export const setAuthFromToken = (token) => {
     const decodedToken = jwtDecode(token);
+
+    const fetchPhoto = async (url) => {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const blob = await response.blob();
+            return blob;
+        } catch (error) {
+            console.error('Error fetching the photo:', error);
+            throw error;
+        }
+    };
+
+    const blobToBase64 = (blob) => {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onloadend = () => resolve(reader.result);
+            reader.onerror = reject;
+            reader.readAsDataURL(blob);
+        });
+    };
+
+    const downloadPhoto = async () => {
+        try {
+            const blob = await fetchPhoto(decodedToken.sub.image);
+
+            // Save to local storage
+            const base64String = await blobToBase64(blob);
+            localStorage.setItem('photo', base64String);
+        } catch (error) {
+            console.error('Error downloading the photo:', error);
+        }
+    };
+
+    downloadPhoto();
     return setAuthAction({
         id: decodedToken.sub.id,
         authStatus: decodedToken.sub.roles,
         name: decodedToken.sub.name,
         surname: decodedToken.sub.surname,
         email: decodedToken.sub.email,
-        photo: decodedToken.sub.image,
+        photo: localStorage.getItem('photo'),
         admin: decodedToken.sub.admin,
     });
 }
