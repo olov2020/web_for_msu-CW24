d520bac39ca982992246d60ed7189b73c249994f
olov2020 <vladimir.8624@mail.ru>
2025-01-24 18:52:13 +0300

    change api for getting all news

--- web_for_msu_front/src/api/newsApi.js
+++ web_for_msu_front/src/api/newsApi.js
@@ -1,7 +1,7 @@
 import {$authHost, $host} from "./axiosApi.js";
 
 export const getAllNews = async () => {
-  const response = await $host.get(`/news/`)
+  const response = await $host.get(`/news/get_all/`)
 
   try {
     return response.data;
