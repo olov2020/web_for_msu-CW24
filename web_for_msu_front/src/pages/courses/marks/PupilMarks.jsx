993248f6379be4896a84f56c9f36fd33dd02fd4e
olov2020 <vladimir.8624@mail.ru>
2025-02-01 00:04:54 +0300

    fix styling pupil marks

--- web_for_msu_front/src/pages/courses/marks/PupilMarks.jsx
+++ web_for_msu_front/src/pages/courses/marks/PupilMarks.jsx
@@ -81,7 +81,7 @@ const PupilMarks = ({courseId}) => {
               <h3>{date}</h3>
               {marks.marks[dateIndex] && marks.marks[dateIndex].length !== 0 && marks.marks[dateIndex].map((mark, markIndex) =>
                 (
-                  <p key={`${dateIndex}-${markIndex}`}>{mark}</p>
+                  <h3 key={`${dateIndex}-${markIndex}`}>{mark}</h3>
                 ))}
             </div>
           ))}
